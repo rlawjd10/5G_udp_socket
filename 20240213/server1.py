63ec8f54843290d51a28866de16e054d8b679ea1
jeongeun Kim <rlawjd10eun@gmail.com>
2024-02-13 10:26:52 +0900

    [FEAT] iptables ip_addr success

--- 20240213/server1.py
+++ 20240213/server1.py
@@ -16,18 +16,14 @@ subprocess.run("docker exec -i -t oai-spgwu /bin/bash -c 'iptables -I FORWARD 1 
 # TSHARK
 def ip_tshark():
     try:
-        # 콘솔에 출력하지 않고 수행만 & 결과값을 result 변수에 저장 & 1개 출력하면 수행 끝(autostop condition)
+        # 1개 출력하면 수행 끝(autostop condition)
         result = os.popen("tshark -i demo-oai -Y '(ip.src==12.1.0.0/16)&&(ip.dst==192.168.0.12)&&(frame.len eq 98)' -T fields -e ip.src -a 'duration:1'").read()
         
-        # checking
+        # ip checking
         print(result.split(",")[-1])
 
         ip_addr = result.split(",")[-1]
-        command = ["docker", "exec", "-i", "-t", "oai-spgwu", "/bin/bash", "-c", f"iptables -A FORWARD -s {ip_addr} -j ACCEPT"]
-        subprocess.run(command, check=True)
-        #subprocess.run(f"docker exec -i -t oai-spgwu /bin/bash -c 'iptables -A FORWARD -s {ip_addr} -j ACCEPT'", shell=True, check=True)
-
-        print("success")
+        subprocess.run(f"docker exec -i -t oai-spgwu /bin/bash -c 'iptables -I FORWARD 1 -j ACCEPT -s {ip_addr}'", shell=True, check=True)
         
         return print("tshark success")
         
@@ -39,9 +35,6 @@ def ip_tshark():
 # SIGINT HANDLER FUNCTION
 def handler(signum, frame):
     print("PRESS CTRL + C")
-
-    # ACCEPT 12.1.0.0/16 (iptables rule)
-    #subprocess.run("docker exec -i -t oai-spgwu /bin/bash -c 'iptables -D FORWARD -s 12.1.0.0/16 -j DROP'", shell=True, check=True)
     
     # iptables reset
     subprocess.run("docker exec -i -t oai-spgwu /bin/bash -c 'iptables -F'", shell=True, check=True)
@@ -63,8 +56,8 @@ print(f"UDP 서버가 {host}:{port}에서 실행 중입니다.")
 while True:
     data, client_address = server_socket.recvfrom(1024)
 
-    print(f"클라이언트로부터 수신: {data}")
     data = data.decode('utf-8')
+    print(f"클라이언트로부터 수신: {data}")
 
     if data.strip() == 'success':
         try:
@@ -76,14 +69,12 @@ while True:
 
 '''
 UDP 서버가 192.168.0.13:3030에서 실행 중입니다.
-클라이언트로부터 수신: b'success'
+클라이언트로부터 수신: success
 Running as user "root" and group "root". This could be dangerous.
 Capturing on 'demo-oai'
-6 
+7 
 12.1.1.2
 
-/bin/bash: line 1: -j: command not found
-Error: Command 'docker exec -i -t oai-spgwu /bin/bash -c 'iptables -A FORWARD -s 12.1.1.2
- -j ACCEPT'' returned non-zero exit status 127.
+tshark success
 ^CPRESS CTRL + C
 '''
